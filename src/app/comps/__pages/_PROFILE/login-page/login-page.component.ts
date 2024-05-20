0d4d4103729f5dd686e8165288bdc15ac9cbc07d
boriskiva <boriskiva3@gmail.com>
2024-05-20 16:17:16 +0300

    login made

--- src/app/comps/__pages/_PROFILE/login-page/login-page.component.ts
+++ src/app/comps/__pages/_PROFILE/login-page/login-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {AuthService} from "../../../../services/api/auth.service";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  public username: string = '';
+  public password: string = '';
 
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
+
+  login() {
+    this.authService.login({username: this.username, password: this.password})
+  }
 }
